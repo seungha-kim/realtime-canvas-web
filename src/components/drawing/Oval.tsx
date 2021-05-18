e286d4bd4787d567a9a6a6848e3bfc149ff222fd
Kim Seungha <seungha.me@gmail.com>
2021-05-18 22:44:05 +0900

    feat: updateEditMode when oval clicked

--- src/components/drawing/Oval.tsx
+++ src/components/drawing/Oval.tsx
@@ -1,21 +1,65 @@
 import { ObjectMaterial } from "../../SystemFacade";
+import { Component } from "preact";
+import { useEditMode } from "../../contexts/EditModeContext";
 
 type Props = {
   material: ObjectMaterial["Oval"];
 };
 
+type InnerProps = Props & {
+  selected: boolean;
+  onSelect: () => void;
+};
+
+type InnerState = {};
+
+class OvalInner extends Component<InnerProps, InnerState> {
+  render() {
+    const { pos_x, pos_y, r_h, r_v } = this.props.material!;
+    return (
+      <>
+        <ellipse
+          onClick={(e) => {
+            e.stopPropagation();
+            this.props.onSelect();
+          }}
+          cx={pos_x}
+          cy={pos_y}
+          rx={r_h}
+          ry={r_v}
+        />
+        {this.props.selected && (
+          <rect
+            x={pos_x - r_h}
+            y={pos_y - r_v}
+            width={r_h * 2}
+            height={r_v * 2}
+            stroke={"red"}
+            fill={"none"}
+          />
+        )}
+      </>
+    );
+  }
+}
+
 function Oval(props: Props) {
-  const material = props.material!;
+  const { editMode, updateEditMode } = useEditMode();
+  const selected =
+    (editMode?.type == "layerAttr" && editMode.id == props.material?.id) ??
+    false;
   return (
-    <ellipse
-      onClick={(e) => {
-        e.stopPropagation();
-        alert(material.name);
+    <OvalInner
+      selected={selected}
+      {...props}
+      onSelect={() => {
+        if (props.material) {
+          updateEditMode({
+            type: "layerAttr",
+            id: props.material.id,
+          });
+        }
       }}
-      cx={material.pos_x}
-      cy={material.pos_y}
-      rx={material.r_h}
-      ry={material.r_v}
     />
   );
 }
