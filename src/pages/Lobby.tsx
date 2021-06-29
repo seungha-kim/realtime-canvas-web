48f90312dd159c0a251439f0204ebbcce19e98c9
Kim Seungha <seungha.me@gmail.com>
2021-06-29 21:42:49 +0900

    feat: create/list files

--- src/pages/Lobby.tsx
+++ src/pages/Lobby.tsx
@@ -6,24 +6,58 @@ type Props = {
 
 type InnerProps = Props & {};
 
-class LobbyInner extends Component<InnerProps> {
+type InnerState = {
+  files: string[] | null;
+};
+
+class LobbyInner extends Component<InnerProps, InnerState> {
   inputRef = createRef<HTMLInputElement>();
 
-  handleJoin = async () => {
-    const fileId = this.inputRef.current!.value;
+  constructor(props: InnerProps) {
+    super(props);
+
+    this.state = {
+      files: null,
+    };
+  }
+
+  handleCreate = async () => {
     try {
-      this.props.onJoin(fileId);
+      const res = await fetch(`//${process.env.WS_HOST}/files`, {
+        method: "POST",
+      });
+      const json = await res.json();
+      this.props.onJoin(json.fileId);
     } catch (e) {
       alert(e);
     }
   };
 
+  componentDidMount() {
+    this.fetchFiles();
+  }
+
+  async fetchFiles() {
+    const res = await fetch(`//${process.env.WS_HOST}/files`);
+    const json = await res.json();
+    this.setState({ files: json });
+  }
+
   render() {
     return (
       <div>
         <div>
-          Join a session: <input ref={this.inputRef} type="text" />
-          <button onClick={this.handleJoin}>Join!</button>
+          <button onClick={this.handleCreate}>Create a session</button>
+        </div>
+        <div>
+          {this.state.files?.map((fileId) => (
+            <div
+              onClick={() => this.props.onJoin(fileId)}
+              style={{ cursor: "pointer" }}
+            >
+              {fileId}
+            </div>
+          ))}
         </div>
       </div>
     );
